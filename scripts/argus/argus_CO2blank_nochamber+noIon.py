2159f20d58afcdc2dcdedb36b3f9a62de8d6ef81
angl_laser <aschaen@gmail.com>
2020-05-25 17:11:40 -0700

    repo updated for analysis bu-AC-A-502

---
+++ scripts/argus/argus_CO2blank_nochamber+noIon.py
@@ -0,0 +1,50 @@
+#===============================================================================
+# EXTRACTION SCRIPT argus_CO2blank_nochamber+noIon.py
+#===============================================================================
+'''
+modifier: 01
+eqtime: 15
+'''
+
+def main():
+        
+    ###########################################    
+    # UA ANGL full analyitcal blank without Ion pump or laser chamber
+    
+    info('Argus VI Ar Extraction Script')
+    
+    #reset valving, pump prep/MS
+    close('1') # Prep ion
+    close('8') # MS inlet
+    open('X') #Prepturbo
+    open('9') # MS ion
+    open('3') # Aux 1
+    close ('2') # Aux 2
+    sleep(10)
+    
+    #close pumps
+    close('X') #Prepturbo
+
+    #laser fire
+    
+    #gas cleanup
+    sleep(cleanup)
+#===============================================================================
+# EXTRACTION SCRIPT argus_CO2blank_nochamber.py
+#===============================================================================
+
+#===============================================================================
+# POST EQUILIBRATION SCRIPT argus_pump_v1turbo_ONLY_noIon.py
+#===============================================================================
+def main():
+    open('X') # Prep turbo
+    open('3') # Aux 1
+    open('2') # Aux 2
+    sleep (300) 
+    
+    
+#===============================================================================
+# POST MEASUREMENT SCRIPT argus_pump_ms.py
+#===============================================================================
+def main():
+    open('9') # MS Ion Pump
---
+++ scripts/argus/argus_pump_v1turbo_ONLY_noIon.py
@@ -0,0 +1,7 @@
+def main():
+    open('X') # Prep turbo
+    open('3') # Aux 1
+    open('2') # Aux 2
+    sleep (300) 
+    
+    
