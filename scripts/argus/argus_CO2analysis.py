f90f3b9a918d1f1b387b6ab7814fceb84e90456a
angl_laser <aschaen@gmail.com>
2020-12-08 17:56:40 -0700

    repo updated for analysis bu-AC-A-1066

--- scripts/argus/argus_CO2analysis.py
+++ scripts/argus/argus_CO2analysis.py
@@ -108,7 +108,8 @@ def main():
     open('X') # Prep turbo
     open('3') # Aux 1
     open('2') # Aux 2
-    sleep (300) 
+    sleep (300)
+    close('X') # Prep Turbo
     open('1') # Prep Ion Pump
     
 #===============================================================================
