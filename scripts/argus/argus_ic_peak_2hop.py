850d44ff5f9892b26da949f8828fb01dfa355a2a
angl_laser <aschaen@gmail.com>
2021-04-23 17:58:52 -0700

    repo updated for analysis ba-01-A-1637

--- scripts/argus/argus_ic_peak_2hop.py
+++ scripts/argus/argus_ic_peak_2hop.py
@@ -19,12 +19,12 @@ EQ_DELAY= 3.0
 
 ACTIVE_DETECTORS = ('H2','H1', 'AX', 'L1', 'L2', 'CDD')
 
-FITS =('Ar40:linear',
+FITS =('Ar40:parabolic',
        'Ar39:linear',
        'Ar38:linear',
        'Ar37:linear',
        'Ar36:linear',
-       'Ar40H1:linear',
+       'Ar40H1:parabolic',
        'Ar39AX:linear',
        'Ar38L1:linear',
        'Ar37L2:linear',
